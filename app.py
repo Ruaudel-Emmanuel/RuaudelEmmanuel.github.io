6c361731ac856e2a34ec38ff14560c837121c220
Emmanuel Ruaudel <ruaudel.emmanuel@orange.fr>
2025-11-09 15:36:23 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # ------------------------------------------------------------------------------
 # Charge la configuration pour l'envoi d'e-mails depuis les variables d'environnement.
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', '587'))
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True').lower() in ['true', 'on', '1']
